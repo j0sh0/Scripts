cc83ca2f1440b863ccd51183f50647831e440edb
Josh <josh@jso.sh>
2019-06-16 01:08:21 +1000

    Update spamhausDROP.sh

--- spamhausDROP.sh
+++ spamhausDROP.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-# jso.sh
-timestamp=$(date +'%d%m%y%_H%M%S') # DayMonthYearHourMinutesSeconds (12062019_050538)
+# Spamhaus DROP list iptables update. - jso.sh
+
+timestamp=$(date +'%d%m%y-%-H:%M:%S') # DayMonthYear-Hour:Minutes:Seconds (150619-23:58:25)
 url="www.spamhaus.org/drop/drop.lasso" # Spamhaus Don't Route Or Peer Lists
+drop=/tmp/drop.lasso.$$.$RANDOM         # tmp file /tmp/<drop>.<lasso>.<CurrentUserPid>.<random number>
 log=/var/log/spamhausDROP.log # Log file <CurrentUserPid>.<timestamp>.<log>
 iptChain="SpamhausDROP" # iptables chain name
 ipregex="(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\/[0-3][0-9])" # regex filter for ip in cidr format
+trap "rm -f $drop" EXIT   # exit trap to clean tmp file
 
 function importDROP { # add ip to chain
 for ipblock in $blocks
@@ -16,11 +19,12 @@ iptables -I INPUT -j $iptChain # add to INPUT chain
 iptables -I OUTPUT -j $iptChain # add to OUTPUT chain
 iptables -I FORWARD -j $iptChain # add to FOWARD chain
 	}
-echo "############################################################################" >> $log
+echo "####################################################################################" >> $log
 echo "$timestamp: Attempting to Apply the newest spamhaus DROP list to iptables..." >> $log
 echo "$timestamp: Process id: $$" >> $log
-curl $url | head -n 4 >> $log
-blocks=$(curl $url | egrep -o $ipregex) # download and cleanup the list using regex
+wget -q -O $drop $url   # Download list
+cat $drop | head -n 4 >> $log
+blocks=$(cat $drop | egrep -o $ipregex) # download and cleanup the list using regex
 iptables -N $iptChain # create new chain
 if [ $? ]; 
 then # if chain already exists then flush and import new rules
